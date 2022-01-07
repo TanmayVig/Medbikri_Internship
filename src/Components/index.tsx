8458e2447d802beb6ac39b970b8b2ca014e884c1
TanmayVig <tanmayvij11@gmail.com>
2022-01-07 16:22:31 +0530

    formatting

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,17 +1,4 @@
-import Button from "./Button";
-import Modal from "./Modal";
-import ModalHeader from "./ModalHeader";
-import ModalFooter from "./ModalFooter";
-import ModalBody from "./ModalBody";
 import Placeholder from "./Placeholder";
 import NotFound from "./NotFound";
 
-export {
-  Button,
-  Modal,
-  Placeholder,
-  ModalBody,
-  ModalFooter,
-  ModalHeader,
-  NotFound,
-};
+export { Placeholder, NotFound };
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,19 +1,27 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 import { NotFound } from "../Components";
-import Page1 from './Page1';
-import Page2 from './Page2';
+import Page1 from "./Page1";
+import Page2 from "./Page2";
 
 const Routes: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/launchpads" component={Page1} />
-        <Route path="/launch/:id" exact component={Page2} />
-        <Route exact path="*" component={NotFound} />
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Redirect exact from="/" to="/launchpads" />
+        <Switch>
+          <Route exact path="/launchpads" component={Page1} />
+          <Route path="/launch/:id" exact component={Page2} />
+          <Route exact path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    </>
   );
 };
 
